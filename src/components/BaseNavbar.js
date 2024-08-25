d9ac5538b4da6d4436d3a839185719536ddafc9c
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-08-26 00:08:24 +0200

    yo

---
+++ src/components/BaseNavbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const menuNavbar = [
+        {
+            name:Accueil,
+            link:"/"
+        },
+        {
+            name:Admin,
+            link:"/admin"
+        }
+
+]
+
+const BaseNavbar = () => {
+    return (
+        <div className="h-22 bg-blue-300 fixed">
+            <nav className="flex justify-between items-center h-full p-4">
+                <ul className="flex items-center space-x-4">
+                    {menuNavbar.map((item, index) => (
+                        <li key={index}>
+                            <a href={item.link}>{item.name}</a>
+                        </li>
+                        ))}
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default BaseNavbar
