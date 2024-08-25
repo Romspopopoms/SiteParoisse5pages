aad17ce8182cd0b9aebca981294f022fe0c42782
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-08-26 00:18:19 +0200

    yo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/HomePage"
 import PreviewPage from './pages/PreviewPage';
+import AdminDashboard from './pages/Admin';
 
 const App = () => {
   return (
@@ -10,6 +11,8 @@ const App = () => {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/PreviewPage" element={<PreviewPage />} />
+              <Route path="/admin" element={<AdminDashboard />} />
+
             </Routes>
           </div>
         </Router>
