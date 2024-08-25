464ab86797e96a208e311c91b4b5cf158359fce6
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-08-26 00:53:23 +0200

    yo

--- api/deploy.js
+++ api/deploy.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.post('/api/deploy', async (req, res) => {
     try {
         const { repoName } = req.body;
+        console.log(`Début du déploiement pour le repo: ${repoName}`);
 
         // Étape 1 : Créer un nouveau projet sur Vercel
         const projectResponse = await fetch('https://api.vercel.com/v9/projects', {
@@ -21,18 +22,21 @@ app.post('/api/deploy', async (req, res) => {
                 name: repoName,
                 gitRepository: {
                     type: 'github',
-                    repo: `Romspopopoms/${repoName}`,  // Utilisez le nom d'utilisateur et le repo
+                    repo: `Romspopopoms/${repoName}`,
                 },
             }),
         });
 
         const projectData = await projectResponse.json();
+        console.log('Réponse de création de projet:', projectData);
 
         if (!projectResponse.ok) {
+            console.error('Erreur lors de la création du projet:', projectData);
             return res.status(500).json({ error: projectData.error.message });
         }
 
         const projectId = projectData.id;
+        console.log(`Projet créé avec ID: ${projectId}`);
 
         // Étape 2 : Déployer le projet nouvellement créé
         const deployResponse = await fetch('https://api.vercel.com/v13/deployments', {
@@ -52,17 +56,21 @@ app.post('/api/deploy', async (req, res) => {
         });
 
         const deployData = await deployResponse.json();
+        console.log('Réponse de déploiement:', deployData);
 
         if (deployResponse.ok) {
+            console.log('Déploiement réussi:', deployData.url);
             return res.status(200).json({ previewUrl: deployData.url });
         } else {
+            console.error('Erreur lors du déploiement:', deployData);
             return res.status(500).json({ error: deployData.error.message });
         }
     } catch (error) {
-        console.error('Server Error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error('Erreur côté serveur:', error);
+        res.status(500).json({ error: 'Erreur Interne du Serveur' });
     }
 });
+
 
 
 module.exports = app;  // Ajoutez cette ligne à la fin de votre fichier `deploy.js`
